55a8b3e92df22731420504fb5cb8dae4797e1d64
yuserysheva <yuserysheva@phoenixit.tu>
2026-03-22 20:05:37 +0300

    поменяла веб версию

--- src/main/resources/static/flutter_service_worker.js
+++ src/main/resources/static/flutter_service_worker.js
@@ -65,7 +65,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "3262c03b9fbf63b1d37bbb146dcbf70a
 "canvaskit/skwasm_heavy.wasm": "b0be7910760d205ea4e011458df6ee01",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "24bc71911b75b5f8135c949e27a2984e",
-"flutter_bootstrap.js": "d7afe012dc55dc5b31a7b272c274e563",
+"flutter_bootstrap.js": "6f2bdd590875b041695c06c89caf2a24",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
